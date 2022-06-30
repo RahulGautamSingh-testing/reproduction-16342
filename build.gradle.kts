66a15c30942dd04f0e961ab2a2f1b3e1163d6d82
null <rahul_ug@cse.nits.ac.in>
2022-06-30 21:48:10 +0530

    Update dependency com.android.tools.build:gradle to v3.6.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.1.3")
+        classpath("com.android.tools.build:gradle:3.6.4")
         classpath(kotlin("gradle-plugin", version = "1.3.70"))
         compileOnly("com.example.foo:tool-types:2022-11-10_123")
         // NOTE: Do not place your application dependencies here; they belong
