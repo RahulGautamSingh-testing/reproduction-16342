acda58a517d22e5ff13e9b5988c4e08f2c71af2a
null <rahul_ug@cse.nits.ac.in>
2022-06-30 20:33:17 +0530

    Update dependency com.android.tools.build:gradle to v7

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.1.3")
+        classpath("com.android.tools.build:gradle:7.2.1")
         classpath(kotlin("gradle-plugin", version = "1.3.70"))
         compileOnly("com.example.foo:tool-types:2022-05-10_55")
         // NOTE: Do not place your application dependencies here; they belong
